0bf83ee50c2b39efa4319eeae0c4abeae2686a32
Tony Morelli <tony@tonymorelli.com>
2014-02-28 11:19:52 -0500

    More testing

--- go.pl
+++ go.pl
@@ -10,7 +10,15 @@
 
 @branches;
 @versions;
+$branchAdded = 0;
+$newBranch;
 
+sub branchUpdated{
+	$branchUpdate = $_[0];
+	#do something with this new branch!!
+}
+
+#################################################################
 sub checkForNewBranches(){
 	$res = `git branch -r`;
 	@tmpBranches = split /\n/, $res;
@@ -37,41 +45,85 @@ sub checkForNewBranches(){
 		}
 		if ($found == 0){
 			print ("New Branch: $tmpBranch\n");
+			$branchAdded = 1;
+			$newBranch = $tmpBranch;
 		}
 	}
 	@branches = @tmpBranches;
-
 }
 
+#################################################################
+sub initVersions(){
+	@versions = ();
+	foreach my $line (@branches) {
+		$version = `git log -1 --format="%H" $line`;
+		chomp($version);
+		push (@versions, $version);
+		print "$line:$version\n";
+	}
+}
+
+#################################################################
 sub checkForNewVersions(){
 	#first time
 	if ($#versions <=0){
-		foreach my $line (@branches) {
-			$version = `git log -1 --format="%H" $line`;
-			chomp($version);
-			push (@versions, $version);
-			print "$line:$version\n";
+		initVersions();
+		return;
+	}
+	$needToInit = 0;
+	foreach my $line (@branches) {
+		if (length($line) <2){
+			continue;
+		}
+		#print("Checking: $line\n");
+		$tmpVersion = `git log -1 --format="%H" $line`;
+		#print("Checked: $line\n");
+		chomp($tmpVersion);
+		#is this version in our version list?
+		#if yes, continue
+		#if no, call branchupdated and set flag to update all versions
+
+		$found = 0;
+		foreach my $v (@versions) {
+			if (length($v) > 2 && length($tmpVersion) > 2){
+				if ($v eq $tmpVersion){
+					$found = 1;
+				}
+			}
+		}
+		if ($found == 0 && length($tmpVersion) > 2) {
+			branchUpdated($line);
+			$needToInit = 1;
 		}
 	}
-
-	if ($#versions != $#branches){
-		#new branch!!
+	if ($needToInit == 1) {
+		initVersions();
 	}
-	
-
 }
 
+
+#################################################################
+sub handleNewBranch(){
+	initVersions();
+	$branchAdded = 0;
+	branchUpdated($newBranch);
+}
+
+#################################################################
 sub main(){
 	while (1==1){
 		checkForNewBranches();
+		if ($branchAdded == 1) {
+			handleNewBranch();
+		}
 		checkForNewVersions();
 		sleep(1);
-
 	}
-
 }
+#################################################################
 main();
 
+#################################################################
 
 
 
